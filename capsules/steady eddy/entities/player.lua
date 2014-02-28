90a49be366d525e8a7c8590f0e4e08d0b4d0c3a9
Mattias Engström <gamermatte@hotmail.com>
2014-02-28 19:00:19 +0100

    addons to steady eddy

--- capsules/steady eddy/entities/player.lua
+++ capsules/steady eddy/entities/player.lua
@@ -16,7 +16,9 @@ function player.new( map, x, y, z )
 
 
 	-- Common variables
-	local width, height = 128, 128
+	self.mx, self.my = self.x, self.y
+
+	local width, height = 128, 32
 	local ox, oy = width/2, height/2
 	local sx, sy = 1, 1
 	local r = 0
@@ -26,22 +28,19 @@ function player.new( map, x, y, z )
 
 
 	-- BUFFER BATCH
-	self.bufferBatch = yama.buffers.newBatch(self.x, self.y, 0)
-	--spriteJumper = yama.buffers.newDrawable(yama.assets.loadImage("jumper"),self.x, self.y, self.z, r, sx, sy, ox, oy )
-
-	--local spritePlatform = yama.buffers.newDrawable(yama.assets.loadImage("platform"), self.x, self.y, 1, 1, sx, sy, 3, 3 )
-
+	self.bufferBatch = yama.buffers.newBatch(self.x, self.y, 1)
+	local spritePlatform = yama.buffers.newDrawable(yama.assets.loadImage("platform"),self.x, self.y, self.z, 1, 1, 1, 1, 1 )
 	
-	--table.insert( self.bufferBatch.data, spritePlatform )
+	table.insert( self.bufferBatch.data, spritePlatform )
 
-
+	--love.mouse.setPosition( x, y )
 	-- Physics
 
 	function self.initialize( properties )
-		self.refreshBufferBatch()
+
 	end
-	--[[ 
-	self.fixtures.main = love.physics.newFixture(love.physics.newBody(  map.world, self.x, self.y, "dynamic"), love.physics.newRectangleShape(60,16), 1)
+	---[[ 
+	self.fixtures.main = love.physics.newFixture(love.physics.newBody(  map.world, self.x, self.y, "dynamic"), love.physics.newRectangleShape(180,10), 1)
 	self.fixtures.main:setGroupIndex( 1 )
 	self.fixtures.main:setCategory( 1 )
 	self.fixtures.main:setMask( 2 )
@@ -53,21 +52,21 @@ function player.new( map, x, y, z )
 	self.fixtures.main:getBody():setInertia( 1 )
 	self.fixtures.main:getBody():setGravityScale( 9 )
 	self.fixtures.main:getBody():setBullet( true )
-	]]
+	--]]
 
 	function self.update( dt )
-		self.x = x
-		self.y = y
-		self.z = z
+		--love.mouse.setPosition( self.x, self.y )
+		--self.mx, self.my = love.mouse.getPosition( )
+		--self.fixtures.main:getBody():setPosition(self.mx, self.my)
 
+		self.updatePosition()
 	end
 	
 	function self.gamepadpressed( button )
 		if button == "a" then
-			self.beginJump()
+			
 		end
 		if button == "y" then
-			self.toggleWeapon()
 		end
 	end
 
@@ -85,24 +84,24 @@ function player.new( map, x, y, z )
 
 		self.bufferBatch = yama.buffers.newBatch(x, y, z)
 
-	--	table.insert( self.bufferBatch.data, spritePlatform )
+		table.insert( self.bufferBatch.data, spritePlatform )
 	end
 
 
 	function self.updatePosition(xn, yn)		
-		x = self.fixtures.main:getBody():getX()
-		y = self.fixtures.main:getBody():getY()
-		r = self.fixtures.main:getBody():getAngle()
+		self.x = self.fixtures.main:getBody():getX()
+		self.y = self.fixtures.main:getBody():getY()
+		self.r = self.fixtures.main:getBody():getAngle()
 
-		--spritePlatform.x = x
-		--spritePlatform.y = y
-		--spritePlatform.z = 0
-		--spritePlatform.r = r
+		spritePlatform.x = self.x
+		spritePlatform.y = self.y
+		--spritePlatform.z = self.0
+		spritePlatform.r = self.r
 
-		self.bufferBatch.x = x
-		self.bufferBatch.y = y
-		self.bufferBatch.z = 0
-		self.bufferBatch.r = r
+		self.bufferBatch.x = self.x
+		self.bufferBatch.y = self.y
+		--self.bufferBatch.z = 0
+		self.bufferBatch.r = self.r
 	end
 
 	function self.draw( )
